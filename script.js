b29064518b9c15a1541ce24327f0880812f6aac5
igorao2802-dev <igorao2802@gmail.com>
2026-03-11 23:15:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// script.js - Starter Kit для практической работы №5
+// Здесь Вы пишите свой код по заданиям!
+
+// Задание 1: Секундомер
+const swDisplay = document.getElementById('stopwatch-display');
+const swStartBtn = document.getElementById('sw-start');
+const swStopBtn = document.getElementById('sw-stop');
+const swResetBtn = document.getElementById('sw-reset');
+
+// TODO: Объявить переменные для секундомера (например, timerIdentifier, totalSeconds)
+
+// TODO: Функция форматирования времени в ЧЧ:ММ:СС
+
+// TODO: Функция старта секундомера
+
+// TODO: Функция остановки секундомера
+
+// TODO: Функция сброса секундомера
+
+// TODO: Привязать обработчики событий к кнопкам
+
+// Задание 2: Обратный отсчёт
+const cdInput = document.getElementById('countdown-seconds');
+const cdStartBtn = document.getElementById('cd-start');
+const cdStopBtn = document.getElementById('cd-stop');
+const cdDisplay = document.getElementById('countdown-display');
+
+// TODO: Объявить переменные для таймера обратного отсчёта
+
+// TODO: Функция старта обратного отсчёта с валидацией
+
+// TODO: Функция остановки обратного отсчёта
+
+// TODO: Привязать обработчики событий к кнопкам
+
+// Задание 3: Диалоговые окна
+const alertBtn = document.getElementById('btn-alert');
+const confirmBtn = document.getElementById('btn-confirm');
+const promptBtn = document.getElementById('btn-prompt');
+const dialogResult = document.getElementById('dialog-result');
+
+// TODO: Функция показа alert
+
+// TODO: Функция показа confirm и вывод результата
+
+// TODO: Функция показа prompt и вывод результата
+
+// TODO: Привязать обработчики событий к кнопкам
